818c606547447cee7fefd8812c070c8eea7ab1b2
David <DavidRayVaughan00@gmail.com>
2014-11-13 11:15:56 -0800

    Added sorted insertion

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /*
     thingRanker
     David Vaughan
-    11/11/2014
+    11/13/2014
 */
 
 
@@ -9,6 +9,7 @@
 #include <set>
 #include <list>
 #include <string>
+#include <deque>
 
 using namespace std;
 
@@ -37,16 +38,17 @@ int listObject::maxIndex = 0;
 
 
 // Prototypes
-void displayList(const list<listObject>& objectList);
+void displayList(const deque<listObject>& objectList);
 void displayHelp();
-void quickInsert(list<listObject>& objectList);
+void quickInsert(deque<listObject>& objectList);
+void sortedInsert(deque<listObject>& objectList);
 
 
 // Main
 int main()
 {
     // The list that will hold all the objects
-    list<listObject> objectList;
+    deque<listObject> objectList;
 
     // Main loop
     string command = "";
@@ -57,6 +59,8 @@ int main()
             displayHelp();
         else if(command == "i")
             quickInsert(objectList);
+        else if(command == "s")
+            sortedInsert(objectList);
         else if(command == "")
             { /* do nothing */ }
         else if(command == "q")
@@ -78,7 +82,7 @@ int main()
 
 
 // Function to display the list
-void displayList(const list<listObject>& objectList)
+void displayList(const deque<listObject>& objectList)
 {
     // Padding
     cout << endl;
@@ -92,7 +96,7 @@ void displayList(const list<listObject>& objectList)
 
     // Print out the list
     int count = 0;
-    for(list<listObject>::const_iterator it = objectList.begin();
+    for(deque<listObject>::const_iterator it = objectList.begin();
                                          it != objectList.end();  ++it)
     {
         cout << count++ << ": "
@@ -112,11 +116,12 @@ void displayHelp()
     cout << "Help and commands:" << endl
          << "h: Print this" << endl
          << "i: Quick insert" << endl
+         << "s: Sorted insert" << endl
          << "q: Quit" << endl;
 }
 
 // Insert objects into the list without sorting
-void quickInsert(list<listObject>& objectList)
+void quickInsert(deque<listObject>& objectList)
 {
     // Grab items until quit
     string myStr = "";
@@ -125,6 +130,57 @@ void quickInsert(list<listObject>& objectList)
         // Make a new object from the string, and insert it
         if(myStr != "")
             objectList.push_back(listObject(myStr));
+
+        // Show the list
+        displayList(objectList);
+
+        // Prompt for a new item
+        cout << "Enter a new item ('/q' to quit):" << endl;
+        cin >> myStr;
+    }
+    while(myStr != "/q");
+
+    cout << endl;
+}
+
+// Insert and sort objects into sorted list
+void sortedInsert(deque<listObject>& objectList)
+{
+    // Grab items until quit
+    string myStr = "";
+    do
+    {
+        // Insert the item
+        if(myStr != "")
+        {
+            listObject myObj(myStr);
+
+            // Binary search for the location
+            int beg = 0,
+                end = objectList.size() - 1;
+            while(beg <= end)
+            {
+                int mid = (beg + end) / 2;
+                listObject thisObj = *(objectList.begin() + mid);
+                
+                // Ask the user where to place the item
+                cout << "Is " << myObj.label
+                     << " more important than "
+                     << thisObj.label << "? (y/n)"
+                     << endl;
+
+                char ans;
+                cin >> ans;
+
+                if(ans == 'y')
+                    end = mid-1;
+                else if(ans == 'n')
+                    beg = mid+1;
+            }
+
+            // Place the item in the found slot
+            objectList.insert(objectList.begin() + beg, myObj);
+        }
 
         // Show the list
         displayList(objectList);
